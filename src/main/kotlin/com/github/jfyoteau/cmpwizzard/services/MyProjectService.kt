89feff117fcc68c9b995bd1775c37518a161885a
GitHub Action <action@github.com>
2024-10-06 05:00:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/jfyoteau/cmpwizzard/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jfyoteau/cmpwizzard/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.jfyoteau.cmpwizzard.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jfyoteau.cmpwizzard.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
